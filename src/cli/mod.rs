b35352916f883695561a51eb4d9aa48754af278e
sasso0101 <git@sasso.dev>
2025-10-19 16:28:46 +0200

    added formatting and linting check

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -90,33 +90,31 @@ fn update() -> Result<()> {
 pub fn main() {
   let cli = Cli::parse();
 
-  if let Some(Commands::Init{}) = &cli.command {
-    
+  if let Some(Commands::Init {}) = &cli.command {
   } else {
-  
     match &cli.command {
       Some(Commands::Configure { file }) => {
         let mut sbatchman = core::Sbatchman::new().expect("Failed to initialize Sbatchman");
         sbatchman
           .import_clusters_configs_from_file(file)
           .expect("Failed to import clusters and configs from file");
-      },
-      Some(Commands::Init{}) => {
+      }
+      Some(Commands::Init {}) => {
         let path = env::current_dir().expect("Failed to get current directory");
         Sbatchman::init(&path).expect("Failed to initialize sbatchman directory");
         println!("✅ Sbatchman initialized successfully!");
-      },
+      }
       Some(Commands::Update {}) => {
         update().expect("Failed to update sbatchman");
-      },
+      }
       Some(Commands::SetClusterName { name }) => {
         let mut sbatchman = core::Sbatchman::new().expect("Failed to initialize Sbatchman");
-        sbatchman.set_cluster_name(name)
+        sbatchman
+          .set_cluster_name(name)
           .expect("Failed to set cluster name in sbatchman configuration");
         println!("✅ Cluster name set to '{}' successfully!", name);
-      },
+      }
       None => {}
     }
   }
-
 }
